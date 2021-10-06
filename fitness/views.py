912aaf4e2251d289319b65f603de7948f44a11fd
manvix404 <71367200+manvix404@users.noreply.github.com>
2021-10-06 19:32:45 +0530

    for gallery,about

--- fitness/views.py
+++ fitness/views.py
@@ -18,6 +18,12 @@ from django.utils.encoding import force_bytes
 def index(request):
     return render(request,'index.html')
 
+def gallery(request):
+    return render(request,'gallery.html')
+
+def about(request):
+    return render(request,'about.html')
+
 def contact(request):
     if request.method=='POST':
         name=request.POST['name']
